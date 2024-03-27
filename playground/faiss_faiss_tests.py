8e60806abab7ed7d03f1efca1b3bb91755ca3ca8
Michael Schmid <michael.schmid@smartandpublic.de>
2024-03-27 12:00:54 +0100

    faiss add docs

--- playground/faiss_faiss_tests.py
+++ playground/faiss_faiss_tests.py
@@ -2,28 +2,30 @@
 from dotenv import load_dotenv
 
 
-from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders.text import TextLoader
+from langchain_community.vectorstores.faiss import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders.pdf import PyPDFLoader
 
 
 
 load_dotenv()
+embeddings = OpenAIEmbeddings()
 
 
 loader = TextLoader("./playground/state_of_the_union.txt")
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings()
+
 db = FAISS.from_documents(docs, embeddings)
 print(db.index.ntotal)
 
-loader = PyPDFLoader("./playground/schankerlaubnis.pdf")
-pages = loader.load_and_split()
-text = pages[0].page_content
-print(text)
+loader = PyPDFLoader(file_path="./playground/schankerlaubnis.pdf")
+documents = loader.load()
+#text = pages[0].page_content
+#db = FAISS.from_documents(docs, embeddings)
+db.add_documents(documents=documents)
 
 
