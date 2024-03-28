c2e4dc916071c87a541d36a71554ccaa61dab31a
Tim Janiak <tim.janiak@smartandpublic.de>
2024-03-28 14:06:52 +0100

    added berater-subgraph

--- playground/departments.py
+++ playground/departments.py
@@ -22,19 +22,67 @@ from langchain_openai import ChatOpenAI
 from langgraph.graph import END, MessageGraph, StateGraph
 
 
-class Berater:
-    def create_berater_graph():
-        pass
+def formatter(state):
+    if state is None or len(state["messages"]) == 0:
+        return "No Messages"
+    return state["messages"][-1].content
 
-    class State:
-        messages: list[str]
+
+class Berater:
+    class BeraterState:
+        messages: Annotated[List[BaseMessage], operator.add]
         next: str
+        current_answer: str
+
+    def create_berater_graph():
+        planer_tool = Planer.planer_invoker()
+
+        def berater_agent(state: __class__.BeraterState):
+            messages = state["messages"]
+            response = planer_tool.invoke(messages)
+            return {"messages": [response], "current_answer": response}
+
+        workflow = StateGraph("beraterState")
+        workflow.add_node("berater", berater_agent)
+        workflow.set_entry_point("berater")
+        workflow.add_edge("berater", END)
+
+        graph = workflow.compile(debug=True)
+        return graph
+
+    def enter_chain(message: str):
+        systemPrompt = """
+            Du bist ein freundlicher Kundenberater. Frage den Planer nach Anweisungen, was zu tun ist.
+        """
+
+        def init(x: str):
+            return {
+                "messages": [
+                    SystemMessage(content=systemPrompt),
+                    HumanMessage(content=x),
+                ]
+            }
+
+        return init(message)
+
+    def berater_invoker():
+        return __class__.enter_chain | __class__.create_berater_graph()
 
 
 class Planer:
     class PlanerState(TypedDict):
         messages: Annotated[List[BaseMessage], operator.add]
         next: str
+
+    systemPrompt = """
+            Du bist ein KI-Planungsagent, der einen wichtigen Berater unterstützt.
+            Deine Aufgabe ist es, dem Berater zu helfen, die Anfrage des Kunden zu verstehen und in notwendige Schritte zu unterteilen.
+            Du hast Zugang zu einem Team von Spezialisten in verschiedenen Bereichen, die weitere domänenspezifische Informationen liefern können.
+            Dein Ziel ist es, den besten Handlungsverlauf zu identifizieren und den Berater durch den Prozess zu führen.
+            Denke daran, der Erfolg des Beraters und die Zufriedenheit des Kunden hängen von deiner strategischen Planung und Führung ab.
+            Antworte in einer Liste einfacher Schritte.
+            (Beispiel: 1. Geheimzahl abfragen 2. Fachspezialisten fragen 3. …)
+        """
 
     def create_planer_graph():
         # TODO: Temperature? Ist "kreativitaet" relevant zum "planen"?
@@ -53,27 +101,32 @@ class Planer:
         graph = workflow.compile(debug=True)
         return graph
 
-    def enter_chain(message: str):
-        systemPrompt = """
-            Du bist ein KI-Planungsagent, der einen wichtigen Berater unterstützt.
-            Deine Aufgabe ist es, dem Berater zu helfen, die Anfrage des Kunden zu verstehen und in notwendige Schritte zu unterteilen.
-            Du hast Zugang zu einem Team von Spezialisten in verschiedenen Bereichen, die weitere domänenspezifische Informationen liefern können.
-            Dein Ziel ist es, den besten Handlungsverlauf zu identifizieren und den Berater durch den Prozess zu führen.
-            Denke daran, der Erfolg des Beraters und die Zufriedenheit des Kunden hängen von deiner strategischen Planung und Führung ab.
-        """
-
+    def enter_chain_direct(message: str):
         def init(x: str):
             return {
                 "messages": [
-                    SystemMessage(content=systemPrompt),
+                    SystemMessage(content=__class__.systemPrompt),
                     HumanMessage(content=x),
                 ]
             }
 
         return init(message)
 
+    def enter_chain(messages: List[BaseMessage]):
+        def init(x: List[BaseMessage]):
+            return {"messages": [SystemMessage(content=__class__.systemPrompt)] + x}
+
+        return init(messages)
+
+    def extract_answer(planer_state: dict):
+        return planer_state["messages"][-1]
+
     def planer_invoker():
-        return __class__.enter_chain | __class__.create_planer_graph()
+        return (
+            __class__.enter_chain
+            | __class__.create_planer_graph()
+            | __class__.extract_answer
+        )
 
 
 class DataManager:
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 from form_helper import initialize_app
 from pdf_loader import loader
-from playground.departments import Planer
+from playground.departments import Berater
 
 initialize_app()
 
@@ -34,38 +34,38 @@ with st.form("chatbot_form"):
     if "current_answer" in st.session_state:
         st.info(st.session_state.current_answer)
 
-with st.form("planner_form"):
+with st.form("berater_form"):
 
     def formatter(state):
         if state is None or len(state["messages"]) == 0:
             return "No Messages"
         return state["messages"][-1].content
 
-    submitted = st.form_submit_button("Submit Planner")
-    planer_invoke = Planer.planer_invoker() | formatter
+    submitted = st.form_submit_button("Submit Berater")
+    berater_invoke = Berater.berater_invoker() | formatter
 
-    if "planer" not in st.session_state:
-        st.session_state["planer"] = {
-            "app": planer_invoke,
+    if "berater" not in st.session_state:
+        st.session_state["berater"] = {
+            "app": berater_invoke,
             "current_answer": None,
         }
 
     text = st.text_area(
-        "Planer:",
-        "Was muss ich tun, um in planerWürzburg ein Fest zu feiern?",
+        "berater:",
+        "Was muss ich tun, um in Würzburg ein Fest zu feiern? Antworte in einem Satz!",
     )
     if submitted:
-        st.session_state.planer["current_answer"] = st.session_state.planer[
+        st.session_state.berater["current_answer"] = st.session_state.berater[
             "app"
         ].invoke(
             text,
             config=config,
         )
     if (
-        "current_answer" in st.session_state.planer
-        and st.session_state.planer.get("current_answer") is not None
+        "current_answer" in st.session_state.berater
+        and st.session_state.berater.get("current_answer") is not None
     ):
-        st.info(st.session_state.planer["current_answer"])
+        st.info(st.session_state.berater["current_answer"])
 
 load_button = st.button("Load Document")
 if load_button:
